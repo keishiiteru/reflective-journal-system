90a1e4793dd3b24e9cd396cc1279beaf02b48c89
Kaycee Lim <kasslim749@gmail.com>
2024-04-20 12:41:16 +0800

    Vue.js, Boostrap - Install Vue.js dependencies - Install and apply Boostrap to welcome.blade.php - Vue component setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
+// Default Laravel bootstrapper, installs axios
 import './bootstrap';
+
+ // Added: Actual Bootstrap JavaScript dependency
+import 'bootstrap';
+
+ // Added: Popper.js dependency for popover support in Bootstrap
+import '@popperjs/core';
+
+import { createApp } from 'vue'
+
+import app from './components/app.vue'
+
+createApp(app).mount("#app")
