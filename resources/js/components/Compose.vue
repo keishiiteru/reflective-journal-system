d2f53bc7c19b6a139e46b97bb49061a0b9a21d02
Kaycee Lim <kasslim749@gmail.com>
2024-05-17 18:28:43 +0800

    Compose entry - Implement API and UI function.

--- resources/js/components/Compose.vue
+++ resources/js/components/Compose.vue
@@ -2,17 +2,43 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Compose</h1>
     </div>
-    <form>
+    <form @submit.prevent="submitForm">
         <div class="form-group mb-3">
             <label class="fs-4">Title</label>
-            <input type="email" class="form-control">
+            <input type="text" class="form-control" v-model="form.title">
         </div>
         <div class="form-group mb-3">
             <label class="fs-4">Content</label>
-            <textarea class="form-control" rows="3"></textarea>
+            <textarea class="form-control" rows="3" v-model="form.content"></textarea>
         </div>
         <div class="d-grid gap-2 col-6 mx-auto">
-            <button class="btn btn-primary" type="button">Submit</button>
+            <button class="btn btn-primary" type="submit">Submit</button>
         </div>
     </form>
 </template>
+
+<script setup>
+    import { ref } from "vue"
+    import { useRouter } from "vue-router"
+
+    const router = useRouter()
+
+    const form = ref({
+        title: '',
+        content: ''
+    })
+
+    const submitForm = async() => {
+        try {
+            const response = await axios.post('/api/entries',form.value)
+            alert(response.data.message)
+            router.push('/');
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
+</script>
+
+
