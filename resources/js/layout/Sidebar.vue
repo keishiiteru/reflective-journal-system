1c11eb2e85c6da5c7178af3121942e0665d31d81
Kaycee Lim <kasslim749@gmail.com>
2025-03-22 10:16:49 +0800

    Separate component of Header and Sidebar

---
+++ resources/js/layout/Sidebar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <div class="position-sticky pt-3">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <router-link class="nav-link" active-class="active" to="/my-entries">
+                    <span><font-awesome-icon icon="fa-solid fa-list" /></span>
+                    My Entries
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" active-class="active" to="/compose">
+                    <span><font-awesome-icon icon="fa-solid fa-pen" /></span>
+                    Compose
+                    </router-link>
+                </li>
+            </ul>
+
+            <!-- <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Saved reports</span>
+            <a class="link-secondary" href="#" aria-label="Add a new report">
+                <span data-feather="plus-circle"></span>
+            </a>
+            </h6>
+            <ul class="nav flex-column mb-2">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Current month
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Last quarter
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Social engagement
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Year-end sale
+                </a>
+            </li>
+            </ul> -->
+        </div>
+    </nav>
+</template>
